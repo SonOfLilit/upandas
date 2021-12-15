706ccf518619e24e30748bca6659c3130445abdd
Aur Saraf <sonoflilit@gmail.com>
2021-12-15 20:58:26 +0200

    use numpy array, build from dict, repr

--- tests/test_series.py
+++ tests/test_series.py
@@ -11,7 +11,20 @@ def test_build_from_ndarray():
     assert s.loc[0] == 0
     assert s.loc[4] == 4
 
+def test_build_from_dict():
+    s = ud.Series({'A': 1, 'B': 2, 'C': 3})
+    assert s.loc['A'] == 1
+    assert s.loc['C'] == 3
+
 def test_no_index_passed():
     s = ud.Series([5, 4, 3])
     assert s.loc[0] == 5
     assert s.loc[1] == 4
+
+def test_repr():
+    s = ud.Series([1, 2, 3], index=['A', 'B', 'C'])
+    assert repr(s) == """\
+A\t1.0
+B\t2.0
+C\t3.0
+dtype:float64"""
--- upandas/__init__.py
+++ upandas/__init__.py
@@ -1,21 +1,36 @@
+import numpy as np
+
 class LocHandler:
     def __init__(self, obj) -> None:
         self.obj = obj
     
     def __getitem__(self, key):
-        return self.obj.data[self.obj.index[key]]
+        return self.obj._loc(key)
 
 class Series:
     def __init__(self, data, index=None):
+        if isinstance(data, dict):
+            index = list(data.keys())
+            data = [data[k] for k in index]
+        data = np.array(data, dtype=np.float64)
         self.data = data
         if index is None:
             index = range(len(self.data))
-        self.index = self._build_index(index)
+        self._build_index(index)
 
-    @staticmethod
-    def _build_index(obj):
-        return {k: i for i, k in enumerate(obj)}
+    def _build_index(self, obj):
+        self.index = {k: i for i, k in enumerate(obj)}
+        self.reverse_index = list(obj)
 
     @property
     def loc(self):
         return LocHandler(self)
+
+    def _loc(self, key):
+        return self.data[self.index[key]]
+
+    def __iter__(self):
+        return ((k, self._loc(k)) for k in self.reverse_index)
+
+    def __repr__(self) -> str:
+        return '\n'.join(f'{idx}\t{v}' for idx, v in self) + f'\ndtype:{self.data.dtype}'
